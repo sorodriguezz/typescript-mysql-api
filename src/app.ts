b5a6bb230802b270bff8c8dcb81873df466cd359
sebastian <s.rodriguezzapata01@gmail.com>
2020-06-11 14:22:47 -0400

    sql_routes

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, {Application} from 'express';
+import morgan from 'morgan';
+
+export class App{
+    
+    private app: Application;
+
+    constructor(private port?: number | string){
+        this.app = express();
+        this.settings();
+        this.middlewares();
+    }
+
+    settings(){
+        this.app.set('port', this.port || process.env.PORT || 3200)
+    }
+
+    middlewares(){
+        this.app.use(morgan('dev'));
+    }
+
+    async listen(){
+        await this.app.listen(this.app.get('port'));
+        console.log('Server on http://localhost:3200');
+    }
+}
---
+++ src/routes/index.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.route('/')
+    .get(res, req) => resizeBy.json;
+
+export default router;
